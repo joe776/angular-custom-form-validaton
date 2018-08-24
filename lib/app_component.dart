def818ae3b96c8bb9c10978581c9dc6e81a2d005
Joerg Schoenfisch <joe776@users.noreply.github.com>
2018-08-24 13:50:46 +0200

    inline relevant styles

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,14 +3,18 @@ import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'my-app',
-  directives: [coreDirectives, formDirectives, ParameterValidator],
+  directives: [
+    coreDirectives,
+    formDirectives,
+    ParameterValidator],
   template: '''
-  <h1>Hello {{name}}</h1>
-  <form #myForm="ngForm">
-    <input type="text" ngControl="myInput" validateParameters />
-  </form>
-  ''',
-)
+    <h1>Hello {{name}}</h1>
+    <form #myForm="ngForm">
+      <input type="text" ngControl="myInput" #myInput="ngForm" [class.valid]="myInput.valid" [class.invalid]="!myInput.valid" validateParameters />
+    </form>''',
+  styles: [
+    ':valid {  border-color: #28a745 !important;  border-width: 3px !important; }',
+    ':invalid {  border-color: #dc3545 !important;  border-width: 3px !important; }'])
 class AppComponent {
   var name = 'Angular';
 }
