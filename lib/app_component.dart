012397f1f51caedf8f710cf514b5f8a70d5104c3
Joerg Schoenfisch <joe776@users.noreply.github.com>
2018-08-24 14:22:30 +0200

    add 'real' validation and make it look nice

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -9,21 +9,20 @@ import 'package:angular_forms/angular_forms.dart';
     ParameterValidator],
   template: '''
     <h1>Hello {{name}}</h1>
-    <form #myForm="ngForm">
-      <input type="text" ngControl="myInput" #myInput="ngForm" validateParameters />
-      <input type="text" ngControl="myRequiredInput" #myRequiredInput="ngForm" required />
-
-      <br><span *ngIf="myInput.valid">myInput control is valid</span>
-      <br><span *ngIf="!myInput.valid">myInput control is invalid</span>
-
-      <br><span *ngIf="myRequiredInput.valid">myRequiredInput control is valid</span>
-      <br><span *ngIf="!myRequiredInput.valid">myRequiredInput control is invalid</span>
-    </form>''',
+    <form #myForm="ngForm"><table><tr>
+      <td>length &gt;= 4 </td><td> <input type="text" ngControl="myInput" #myInput="ngForm" validateParameters /></td>
+      <td *ngIf="myInput.valid">control is valid</td>
+      <td *ngIf="!myInput.valid">control is invalid</td>
+      </tr><tr>
+      <td>required </td><td> <input type="text" ngControl="myRequiredInput" #myRequiredInput="ngForm" required /></td>
+      <td *ngIf="myRequiredInput.valid">control is valid</td>
+      <td *ngIf="!myRequiredInput.valid">control is invalid</td>
+    </tr></table></form>''',
   styles: [
     ':valid {  border-color: #28a745;  border-width: 3px; }',
     ':invalid {  border-color: #dc3545;  border-width: 3px; }'])
 class AppComponent {
-  var name = 'Angular';
+  var name = 'Custom Form Validation';
 }
 
 @Directive(selector: '[validateParameters]', providers: [
@@ -33,6 +32,10 @@ class ParameterValidator extends Validator {
   @override
   Map<String, dynamic> validate(AbstractControl control) {
     print('in validator');
-    return {'invalid': 'for some reason...'};
+    final val = control.value;
+    if (val == null || val is String && val.length < 4) {
+      return {'invalid': 'for some reason...'};
+    }
+    return null;
   }
 }
